5d5a0228aa1cc56a158201fc585a2ce22e51ffe2
mihai75 <mihai_nicolae@Mihais-MacBook-Pro.local>
2024-03-17 23:33:09 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify
+# from handlers import FileHandler, OutlierHandler
+
+from handlers import FileHandler, OutlierHandler
+
+app = Flask(__name__)
+
+"""
+/ endpoint. Accepts JSON requests.
+Expect stock_exchange and file_paths params in payload
+Create a CSV file for each file_paths provided in "output_files" directory if there are any outliers
+"""
+@app.route('/', methods=['POST'])
+def process_files():
+    # Handle if not JSON
+    if not request.is_json:
+        return jsonify({'message': 'Application accepts JSON only.'}), 400
+
+    # Extract data & handle errors
+    data = request.get_json()
+
+    if 'file_path' not in data:
+        return jsonify({'message': 'file_path not provided'}), 400
+    if 'stock_exchange' not in data:
+        return jsonify({'message': 'stock_exchange not provided.'}), 400
+
+    stock_exchange = data['stock_exchange']
+    file_paths = data['file_path']
+
+    if not stock_exchange:
+        return jsonify({'message': 'stock_exchange is empty.'}), 400
+    if not file_paths:
+        return jsonify({'message': 'file_path is empty.'}), 400
+
+    # Process each file in stock exchange
+    outliers = []
+
+    for file_path in file_paths[:2]:
+        file_handler = FileHandler(f"{stock_exchange}/{file_path}.csv")
+        df, fn = file_handler.extract_dp()
+
+        outlier_handler = OutlierHandler(df, fn)
+        outlier = outlier_handler.detect_outliers()
+        outliers.append(outlier)
+
+    # Response
+    return jsonify({'message': outliers}), 200
+
+if __name__ == "__main__":
+    app.run()
---
+++ handlers.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import random
+import numpy as np
+
+"""
+Class that handle CSV file
+It takes in the CSV file/s path, read the file and return 30 consecutive DP from a random TimeStamp
+"""
+class FileHandler:
+    def __init__(self, filePath: str):
+        self.filePath = filePath
+        self.data = []
+
+    def __str__(self):
+        return "Class which handle .CSV file."
+
+    # Method that read CSV file
+    def read_file(self) -> None:
+        try:
+            self.data = pd.read_csv(self.filePath)
+            if self.data.empty:
+                 raise ValueError('The CSV file is empty.')
+        except FileNotFoundError:
+            raise "The specified CSV file does not exist."
+        except pd.errors.ParserError:
+            raise "Invalid CSV format."
+        except Exception as e:
+            raise f"An unexpected error occurred: {e}"
+
+
+    # Method that read file and return 30 consecutive data points (from a random TS)
+    def extract_dp(self) -> pd.DataFrame:
+        self.read_file()
+
+        if len(self.data) < 30:
+            raise ValueError("File contains less than 30 data points")
+
+        start_index = random.randint(0, len(self.data) - 30)
+        sample_data = self.data.iloc[start_index:start_index + 30]
+        
+        fileName = self.filePath.split("/")[1]
+        
+        return sample_data, fileName
+
+
+"""
+Class that detects outliers
+It takes in 30 data points and return outliers
+Outlier definition (Threshold): Any datapoint that is over 2 standard deviations beyond the mean of the 30 sampled data points.
+Mean definition: 
+Standard deviation:
+"""
+class OutlierHandler:
+    def __init__(self, df, fileName):
+        self.df = df
+        self.prices = []
+        self.timestamps = []
+        self.outliers = []
+        self.fileName = fileName
+
+    def __str__(self):
+        return "Class which Handle outliers"
+
+    # Method that extract DF timestamps,prices into List
+    def extract_data(self) -> None:
+        self.timestamps = self.df.iloc[:, 1].tolist()
+        self.prices = self.df.iloc[:, 2].tolist()
+
+    # Method that write CSV file with detected outliers
+    def write_outlier_csv(self):
+            outliers_df = pd.DataFrame(self.outliers, columns=['Stock-ID', 'Timestamp', 'Actual Price', 'Mean', 'Price - Mean', '% Deviation'])
+
+            try:
+                outliers_df.to_csv(f"output_files/outliers_{self.fileName}", index=False)
+                print(f"Outliers written to 'outliers_{self.fileName}' file successfully.")
+            except Exception as e:
+                raise f"An error occurred while writing outliers to CSV file: {e}" 
+
+    # Method that detects outliers
+    def detect_outliers(self) -> list:
+        self.extract_data()
+        prices = [float(price) for price in self.prices]
+
+        mean = np.mean(self.prices)
+        std_dev = np.std(self.prices)
+        threshold = mean + (2 * std_dev)
+
+        # Detect outliers, calculate deviation & append data to outliers[]
+        for i in range(len(prices)):
+            if prices[i] > threshold:
+                deviation = (prices[i] - mean) / mean * 100
+                self.outliers.append([self.df.iloc[i, 0], self.timestamps[i], prices[i], mean, prices[i] - mean, deviation])
+
+        # If there are any outliers, write new CSV file
+        if self.outliers:
+            self.write_outlier_csv()
+            return f"Outliers written to 'outliers_{self.fileName}' file successfully." 
+        else:
+            return f"No outliers detected in {self.fileName}"
